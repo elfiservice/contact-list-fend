76c6dc09c5cea1d3d0a7ba46ba9c8e131292ca62
Armando Jr <elfiservice@hotmail.com>
2018-04-19 07:18:13 -0300

    start structure to App

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-    
-    return <ol>
-        {people.map((person, index) => (
-          <li key={person.index}> {person.name} </li>
-        ))}
-      </ol>
-  }
-}
 
 class App extends Component {
   render() {
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My Contact List!</h1>
-        </header>
-        <p className="App-intro">
-          <ContactList contacts={[
-            {name: 'armando'},
-            {name: 'Ana lidia'},
-            {name: 'lidia'}
-          ]}  />
-
-           <ContactList contacts={[
-            {name: 'Kaka'},
-            {name: 'Gibaia'},
-            {name: 'Holyudi'}
-          ]}  />
-        </p>
-      </div>
-    );
   }
 }
+
+
 
 export default App;
