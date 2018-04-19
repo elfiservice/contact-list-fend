06acaba5c2a8766303fad65ca8b3f69fedc2f3a9
Armando Jr <elfiservice@hotmail.com>
2018-04-19 07:30:28 -0300

    feat: provided files and assets to start App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+
+
+
 class App extends Component {
   render() {
 
+    return (
+      <div>
+       hello Mundo !
+      </div>
+    );
   }
 }
-
-
 
 export default App;
