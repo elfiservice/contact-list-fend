054bb599efceaf8bd938ebfeaef1dfed62503746
Armando Jr <elfiservice@hotmail.com>
2018-04-23 06:06:11 -0300

    feat: using default React patterns

--- src/App.js
+++ src/App.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react';
 import ContactList from './ContactList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        {
+          "id": "ryan",
+          "name": "Ryan Florence",
+          "email": "ryan@reacttraining.com",
+          "avatarURL": "http://localhost:5001/ryan.jpg"
+        },
+        {
+          "id": "michael",
+          "name": "Michael Jackson",
+          "email": "michael@reacttraining.com",
+          "avatarURL": "http://localhost:5001/michael.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "email": "tyler@reacttraining.com",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+      ]
+    }
 
-  state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    this.removeContact = this.removeContact.bind(this); //throw the reference of App class into the removeContact function to be used in this.setState()
   }
 
-  removeContact = (contact) => {
+  removeContact(contact) {
     this.setState((currentStateList) => ({
       //stay on the new list array only those who have diferent id 
       contacts: currentStateList.contacts.filter( c => c.id !== contact.id )
