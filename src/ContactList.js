c8ceb282e0564e47304a495ee6be9286c4a43538
Armando Jr <elfiservice@hotmail.com>
2018-04-23 06:47:35 -0300

    refactore: creted input search using State to controller

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-//Functional Component - using because Just return a Render a UI
-function ContactList(props) {
-    const listContacts = props.list;
-    return (
-        <ol className="contact-list">
-        {listContacts.map((contact) => {
-            return (<li key={contact.id} className='contact-list-item'>
-                <div className='contact-avatar' style={{
-                    backgroundImage: `url(${contact.avatarURL})`
-                }} />
-                <div className='contact-details'> 
-                    <p>{contact.name}</p>
-                    <p>{contact.email}</p>
+class ContactList extends Component {
+    //typing the props of parent Component 
+    static propTypes = {
+        list: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        valueInputSearch: ''
+    }
+
+    handleQuery = (newQuery) => {
+        this.setState({
+            valueInputSearch: newQuery.trim()
+        })
+    }
+
+    render() {
+        const listContacts = this.props.list;
+        return (
+            <div className="list-contacts">
+                {JSON.stringify(this.state)}
+                <div className="list-contacts-top">
+                    <input 
+                        className="search-contacts"
+                        type="text"
+                        placeholder="Search a contact"
+                        value={this.state.valueInputSearch}
+                        onChange={(event) => this.handleQuery(event.target.value)}
+                    />
                 </div>
-                <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'> Remove </button>
-            
-            </li>)
-        })}
-    </ol>
+                <ol className="contact-list">
+                    {listContacts.map((contact) => {
+                        return (<li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }} />
+                            <div className='contact-details'> 
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'> Remove </button>
+                        
+                        </li>)
+                    })}
+                </ol>
+            </div>
 
-    )
+
+    
+        )
+    }
 }
 
-//typing the props of parent Component 
-ContactList.propTypes = {
-    list: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
-}
+
+
 
 export default ContactList;
