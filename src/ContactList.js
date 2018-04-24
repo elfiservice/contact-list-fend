f611c847dd9acaa1deb979f595b2074017782bd9
Armando Jr <elfiservice@hotmail.com>
2018-04-24 07:01:51 -0300

    feat: provided input to filter contacts

--- src/ContactList.js
+++ src/ContactList.js
@@ -6,6 +6,12 @@ class ContactList extends Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            valueInputSearch: ''
+        }
+
+        this.handleQuery = this.handleQuery.bind(this);
     }
 
     //typing the props of parent Component 
@@ -14,24 +20,44 @@ class ContactList extends Component {
         onDeleteContact: PropTypes.func.isRequired
     }
 
+    handleQuery(newQuery) {
+        this.setState({ valueInputSearch: newQuery.trim() })
+    }
+
     render() {
         const listContacts = this.props.list;
         return (
-            <ol className="contact-list">
-            {listContacts.map((contact) => {
-                return (<li key={contact.id} className='contact-list-item'>
-                    <div className='contact-avatar' style={{
-                        backgroundImage: `url(${contact.avatarURL})`
-                    }} />
-                    <div className='contact-details'> 
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'> Remove </button>
-                
-                </li>)
-            })}
-        </ol>
+
+            <div className="list-contacts">
+            {JSON.stringify(this.state.valueInputSearch)}
+                <div className="list-contacts-top">
+                    <input 
+                        className="search-contacts"
+                        type="text"
+                        placeholder="Search a contact"
+                        value={this.state.valueInputSearch}
+                        onChange={(event) => this.handleQuery(event.target.value)}
+                    />
+                </div>
+
+                <ol className="contact-list">
+                    {listContacts.map((contact) => {
+                        return (<li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }} />
+                            <div className='contact-details'> 
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'> Remove </button>
+                        
+                        </li>)
+                    })}
+                </ol>
+            </div>
+
+
     
         )
     }
