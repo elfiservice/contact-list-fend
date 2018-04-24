9ffa1feb7dbb007783ff7e5357a6168dffeebc89
Armando Jr <elfiservice@hotmail.com>
2018-04-24 06:53:35 -0300

    refactore: of functional comp to class compt

--- src/ContactList.js
+++ src/ContactList.js
@@ -2,32 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 //Functional Component - using because Just return a Render a UI
-function ContactList(props) {
-    const listContacts = props.list;
-    return (
-        <ol className="contact-list">
-        {listContacts.map((contact) => {
-            return (<li key={contact.id} className='contact-list-item'>
-                <div className='contact-avatar' style={{
-                    backgroundImage: `url(${contact.avatarURL})`
-                }} />
-                <div className='contact-details'> 
-                    <p>{contact.name}</p>
-                    <p>{contact.email}</p>
-                </div>
-                <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'> Remove </button>
-            
-            </li>)
-        })}
-    </ol>
+class ContactList extends Component {
 
-    )
+    constructor(props) {
+        super(props)
+    }
+
+    //typing the props of parent Component 
+    static propTypes = {
+        list: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    render() {
+        const listContacts = this.props.list;
+        return (
+            <ol className="contact-list">
+            {listContacts.map((contact) => {
+                return (<li key={contact.id} className='contact-list-item'>
+                    <div className='contact-avatar' style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }} />
+                    <div className='contact-details'> 
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'> Remove </button>
+                
+                </li>)
+            })}
+        </ol>
+    
+        )
+    }
+
 }
 
-//typing the props of parent Component 
-ContactList.propTypes = {
-    list: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
-}
+
 
 export default ContactList;
